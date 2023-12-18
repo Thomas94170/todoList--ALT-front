0b4c1eac1b37c5d1cd1bf195338d6fab53341b53
Thomas Thirion <thirionthomas68100@gmail.com>
2023-12-18 12:43:54 +0100

    feat : canceled button checked in formcreation

--- src/components/form/formcreation.js
+++ src/components/form/formcreation.js
@@ -39,7 +39,6 @@ function Formcreation() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Envoyer les données du formulaire à l'API pour créer la tâche
     fetch("http://localhost:5555/work/setWorks", {
       method: "POST",
       headers: {
@@ -57,126 +56,144 @@ function Formcreation() {
       );
   };
 
-  return (
-    <form onSubmit={handleSubmit} className="mt-5">
-      <div className="mb-3">
-        <label htmlFor="created_by">Edité par:</label>
-        <select
-          id="created_by"
-          name="created_by"
-          value={formData.created_by}
-          onChange={handleChange}
-          required
-        >
-          <option value="" disabled>
-            Sélectionnez un utilisateur
-          </option>
-          {users.map((user) => (
-            <option key={user._id} value={user.nickname}>
-              {user.nickname}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="name">Nom de la tâche:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div class="mb-3">
-        <label htmlFor="description">Description:</label>
-        <input
-          id="description"
-          name="description"
-          value={formData.description}
-          onChange={handleChange}
-          required
-        ></input>
-      </div>
-      <div class="mb-3">
-        <label htmlFor="category">Catégorie:</label>
-        <input
-          type="text"
-          id="category"
-          name="category"
-          value={formData.category}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div class="mb-3">
-        <label htmlFor="priority">Priorité:</label>
-        <select
-          id="priority"
-          name="priority"
-          value={formData.priority}
-          onChange={handleChange}
-          required
-        >
-          <option value="" disabled>
-            Sélectionnez la priorité
-          </option>
-          <option value="Haute">Haute</option>
-          <option value="Moyenne">Moyenne</option>
-          <option value="Basse">Basse</option>
-        </select>
-      </div>
-      <div class="mb-3">
-        <label htmlFor="status">Statut:</label>
-        <select
-          id="status"
-          name="status"
-          value={formData.status}
-          onChange={handleChange}
-          required
-        >
-          <option value="" disabled>
-            Sélectionnez le statut
-          </option>
-          <option value="À faire">À faire</option>
-        </select>
-      </div>
-      <div class="mb-3">
-        <label htmlFor="due_time">Date d'échéance:</label>
-        <input
-          type="date"
-          id="due_time"
-          name="due_time"
-          value={formData.due_time}
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="created_on">Date de création:</label>
-        <input
-          type="text"
-          id="created_on"
-          name="created_on"
-          value={formData.created_on}
-          readOnly
-        />
+  const handleCancel = () => {
+    setFormData({
+      created_by: "",
+      name: "",
+      description: "",
+      category: "",
+      status: "",
+      priority: "",
+      due_time: "",
+      created_on: getCurrentDate(),
+      updated: getCurrentDate(),
+    });
+  };
 
-        <label htmlFor="updated">Mise à jour:</label>
-        <input
-          type="text"
-          id="updated"
-          name="updated"
-          value={formData.updated}
-          readOnly
-        />
+  return (
+    <div className="card">
+      <div className="card-body">
+        <form onSubmit={handleSubmit} className="mt-5">
+          <div className="mb-3">
+            <label htmlFor="created_by">Edité par:</label>
+            <select
+              id="created_by"
+              name="created_by"
+              value={formData.created_by}
+              onChange={handleChange}
+              required
+            >
+              <option value="" disabled>
+                Sélectionnez un utilisateur
+              </option>
+              {users.map((user) => (
+                <option key={user._id} value={user.nickname}>
+                  {user.nickname}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="name">Nom de la tâche:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div class="mb-3">
+            <label htmlFor="description">Description:</label>
+            <input
+              id="description"
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+              required
+            ></input>
+          </div>
+          <div class="mb-3">
+            <label htmlFor="category">Catégorie:</label>
+            <input
+              type="text"
+              id="category"
+              name="category"
+              value={formData.category}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div class="mb-3">
+            <label htmlFor="priority">Priorité:</label>
+            <select
+              id="priority"
+              name="priority"
+              value={formData.priority}
+              onChange={handleChange}
+              required
+            >
+              <option value="" disabled>
+                Sélectionnez la priorité
+              </option>
+              <option value="Haute">Haute</option>
+              <option value="Moyenne">Moyenne</option>
+              <option value="Basse">Basse</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <label htmlFor="status">Statut:</label>
+            <select
+              id="status"
+              name="status"
+              value={formData.status}
+              onChange={handleChange}
+              required
+            >
+              <option value="" disabled>
+                Sélectionnez le statut
+              </option>
+              <option value="À faire">À faire</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <label htmlFor="due_time">Date d'échéance:</label>
+            <input
+              type="date"
+              id="due_time"
+              name="due_time"
+              value={formData.due_time}
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="created_on">Date de création:</label>
+            <input
+              type="text"
+              id="created_on"
+              name="created_on"
+              value={formData.created_on}
+              readOnly
+            />
+
+            <label htmlFor="updated">Mise à jour:</label>
+            <input
+              type="text"
+              id="updated"
+              name="updated"
+              value={formData.updated}
+              readOnly
+            />
+          </div>
+          <button type="submit" class="btn btn-success">
+            Valider
+          </button>
+          <button type="button" class="btn btn-danger" onClick={handleCancel}>
+            Annuler
+          </button>
+        </form>
       </div>
-      <button type="submit" class="btn btn-success">
-        Validate
-      </button>
-      <button type="button" class="btn btn-danger">
-        Cancel
-      </button>
-    </form>
+    </div>
   );
 }
 
